8546f091dfaaaf4b1a157b0342373e0ec6896e91
심유정 <shimuniv@gmail.com>
2022-01-10 11:50:40 +0900

    model 2

--- src/com/javaex/controller/GuestbookController.java
+++ src/com/javaex/controller/GuestbookController.java
@@ -1,5 +1,26 @@
 package com.javaex.controller;
 
-public class GuestbookController {
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/GuestbookController")
+public class GuestbookController extends HttpServlet {
+	
+    public GuestbookController() {
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
 
 }
